afddee0b7614eb72a7ecb75017f2f0e783aca64c
S1787616 <S1787616@S0085CG91707HK.student.HISD.ORG>
2024-04-26 10:30:52 -0500

    main changes

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+
+
+	import javax.swing.*;
+	import java.awt.*;
+	import java.awt.event.*;
+
+	public class Main extends JFrame{
+		private static final int WIDTH =1350;
+		private static final int HEIGHT=700;
+		
+		public Main () {
+			super("Tag!");
+			setSize(WIDTH, HEIGHT);
+			Game play = new Game();
+			((Component) play).setFocusable(true);
+			setBackground(Color.WHITE);
+			getContentPane().add(play);
+			
+			setVisible(true);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+		}
+		
+
+		public static void main(String[] args) {
+			Main run = new Main();
+			
+
+		}
+
+
+	}
+
